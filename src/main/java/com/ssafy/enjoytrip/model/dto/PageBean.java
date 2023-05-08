36b174a3535f49aa8478077bd828320892037fe6
옥수빈 <dhrtnqls0535@naver.com>
2023-05-08 14:51:03 +0900

    user & board(작성, 기준별 조회) 복구

--- src/main/java/com/ssafy/enjoytrip/model/dto/PageBean.java
+++ src/main/java/com/ssafy/enjoytrip/model/dto/PageBean.java
@@ -4,34 +4,42 @@ import java.io.Serializable;
 
 import io.swagger.annotations.ApiParam;
 
-/** UI 화면 페이지에 대한 정보를 표시하는 클래스  */
-public class PageBean implements Serializable{
-	/**검색 조건*/
-	@ApiParam(value="검색 조건")
+/** UI 화면 페이지에 대한 정보를 표시하는 클래스 */
+public class PageBean implements Serializable {
+	/** 검색 조건 */
+	@ApiParam(value = "검색 조건")
 	private String key;
-	/**검색 단어*/
-	@ApiParam(value="검색어")
+	/** 검색 단어 */
+	@ApiParam(value = "검색어")
 	private String word;
-	/**페이징 처리에 대한 link정보*/
+	/** 페이징 처리에 대한 link정보 */
 	private String pageLink;
-	/**현재 페이지 번호*/
-	@ApiParam(value="현재 페이지", required = true)
+
+	private String sortKey;
+
+	/** 현재 페이지 번호 */
+	@ApiParam(value = "현재 페이지", required = true)
 	private int pageNo;
-	/**한 페이지에 보여주 content 개수*/
+	/** 한 페이지에 보여줄 content 개수 */
 	private int interval = 5;
-	/**페이지 시작 번호*/
-	private int start=0;
-	public PageBean() {	}
+	/** 페이지 시작 번호 */
+	private int start = 0;
+
+	public PageBean() {
+	}
+
 	public PageBean(String key, String word, int pageNo) {
 		setKey(key);
 		setWord(word);
 		setPageNo(pageNo);
 	}
+
 	public PageBean(String key, String word, String pageNo) {
 		setKey(key);
 		setWord(word);
 		setPageNo(pageNo);
 	}
+
 	private void setPageNo(String pageNo) {
 		try {
 			this.pageNo = Integer.parseInt(pageNo);
@@ -39,72 +47,81 @@ public class PageBean implements Serializable{
 			this.pageNo = 1;
 		}
 	}
+
 	public String getKey() {
 		return key;
 	}
+
 	public String getKey(String k) {
-		if(key!=null && key.equals(k)) {
+		if (key != null && key.equals(k)) {
 			return "selected='selected'";
-		}else {
+		} else {
 			return "";
 		}
 	}
+
 	public void setKey(String key) {
 		this.key = key;
 	}
+
 	public String getWord() {
 		return word;
 	}
+
 	public void setWord(String word) {
 		this.word = word;
 	}
-	
+
+	public String getSortKey() {
+		return sortKey;
+	}
+
+	public void setSortKey(String sortKey) {
+		this.sortKey = sortKey;
+	}
+
 	public String getPageLink() {
 		return pageLink;
 	}
+
 	public void setPageLink(String pageLink) {
 		this.pageLink = pageLink;
 	}
+
 	public int getPageNo() {
 		return pageNo;
 	}
+
 	public void setPageNo(int pageNo) {
 		this.pageNo = pageNo;
 	}
+
 	public int getInterval() {
 		return interval;
 	}
+
 	public void setInterval(int interval) {
 		this.interval = interval;
 	}
+
 	public int getStart() {
-		if(pageNo>1) {
-			return start = (pageNo-1)*interval;
-		}else {
+		if (pageNo > 1) {
+			return start = (pageNo - 1) * interval;
+		} else {
 			return 0;
 		}
 	}
+
 	public void setStart(int start) {
 		this.start = start;
 	}
+
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("PageBean [key=").append(key).append(", word=").append(word).append(", pagelink=")
-				.append(pageLink).append(", pageNo=").append(pageNo).append(", interval=").append(interval)
-				.append(", start=").append(start).append("]");
-		return builder.toString();
+		return "PageBean [key=" + key + ", word=" + word + ", pageLink=" + pageLink + ", sortKey=" + sortKey
+				+ ", pageNo=" + pageNo + ", interval=" + interval + ", start=" + start + "]";
 	}
+	
+	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/com/ssafy/enjoytrip/model/service/BoardServiceImpl.java
+++ src/main/java/com/ssafy/enjoytrip/model/service/BoardServiceImpl.java
@@ -1,71 +1,71 @@
-//package com.ssafy.enjoytrip.model.service;
-//
-//import java.util.List;
-//
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import com.ssafy.enjoytrip.model.dao.BoardDao;
-//import com.ssafy.enjoytrip.model.dto.Board;
-//import com.ssafy.enjoytrip.model.dto.BookException;
-//import com.ssafy.enjoytrip.model.dto.PageBean;
-//import com.ssafy.enjoytrip.util.PageUtility;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//@Service
-//public class BoardServiceImpl implements BoardService {
-//
-//	private final BoardDao boardDao;
-//
-//	@Transactional
-//	@Override
-//	public void writeArticle(Board boardDto) throws Exception {
-//		boardDao.writeArticle(boardDto);
-//	}
-//
-//	@Transactional(readOnly = true)
-//	@Override
-//	public List<Board> listArticle(PageBean bean) throws Exception {
-//		try {
-//			int total = boardDao.totalCount(bean);
-//			PageUtility page = new PageUtility(bean.getInterval(), total, bean.getPageNo(), "");
-//			bean.setPageLink(page.getPageBar());  //paging 처리한 html 링크를 추출하기 
-//			return boardDao.listArticle(bean);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			throw new BookException("책 목록 정보를 조회 하는 중 오류 발생!!!");
-//		}
-//	}
-//
-//	@Transactional
-//	@Override
-//	public Board getArticle(int articleNo) throws Exception {
-//		return boardDao.getArticle(articleNo);
-//	}
-//
-//	@Transactional
-//	@Override
-//	public void updateHit(int articleNo) throws Exception {
-//		boardDao.updateHit(articleNo);
-//		
-//	}
-//
-//	@Transactional
-//	@Override
-//	public void modifyArticle(Board boardDto) throws Exception {
-//		boardDao.modifyArticle(boardDto);
-//		
-//	}
-//
-//	@Transactional
-//	@Override
-//	public void deleteArticle(int articleNo) throws Exception {
-//		boardDao.deleteArticle(articleNo);
-//		
-//	}
-//
-//}
+package com.ssafy.enjoytrip.model.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.enjoytrip.model.dao.BoardDao;
+import com.ssafy.enjoytrip.model.dto.Board;
+import com.ssafy.enjoytrip.model.dto.BookException;
+import com.ssafy.enjoytrip.model.dto.PageBean;
+import com.ssafy.enjoytrip.util.PageUtility;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	private final BoardDao boardDao;
+
+	@Transactional
+	@Override
+	public void writeArticle(Board boardDto) throws Exception {
+		boardDao.writeArticle(boardDto);
+	}
+
+	@Transactional(readOnly = true)
+	@Override
+	public List<Board> listArticle(PageBean bean) throws Exception {
+		try {
+			int total = boardDao.totalCount(bean);
+			PageUtility page = new PageUtility(bean.getInterval(), total, bean.getPageNo(), "");
+			bean.setPageLink(page.getPageBar()); //paging 처리한 html 링크를 추출하기 
+			return boardDao.listArticle(bean);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new BookException("책 목록 정보를 조회 하는 중 오류 발생!!!");
+		}
+	}
+
+	@Transactional
+	@Override
+	public Board getArticle(int articleNo) throws Exception {
+		return boardDao.getArticle(articleNo);
+	}
+
+	@Transactional
+	@Override
+	public void updateHit(int articleNo) throws Exception {
+		boardDao.updateHit(articleNo);
+
+	}
+
+	@Transactional
+	@Override
+	public void modifyArticle(Board boardDto) throws Exception {
+		boardDao.modifyArticle(boardDto);
+
+	}
+
+	@Transactional
+	@Override
+	public void deleteArticle(int articleNo) throws Exception {
+		boardDao.deleteArticle(articleNo);
+
+	}
+
+}
