ac992baef6807fea4d1c007f674bc1794014e2ad
dkgusdkfk <dkgusdkfk@gmail.com>
2023-05-12 17:48:51 +0900

    rest api 형식으로 변경

--- src/main/java/com/ssafy/enjoytrip/controller/BoardController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/BoardController.java
@@ -8,11 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ssafy.enjoytrip.model.dto.Board;
@@ -20,43 +23,49 @@ import com.ssafy.enjoytrip.model.dto.PageBean;
 import com.ssafy.enjoytrip.model.dto.User;
 import com.ssafy.enjoytrip.model.service.BoardService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/article")
+@Api("게시판 컨트롤러  API V1")
 public class BoardController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+	
 	@Autowired
 	private BoardService boardService;
 
+	@ApiOperation(value = "게시판 글목록", notes = "모든 게시글의 정보를 반환한다.")
 	@GetMapping("/list")
-	public String boardList(PageBean bean, Model model) throws Exception {
+	public ResponseEntity<List<Board>> listArticle(PageBean bean) throws Exception {
 		log.debug("articleslist....................pageBean:{}", bean);
 		List<Board> articles = boardService.listArticle(bean);
 		log.debug("articleslist....................{}", articles);
 		log.debug("sortKey....................{}", bean.getSortKey());
-		model.addAttribute("articles", articles);
-		return "board/InfoBoard";
+		return new ResponseEntity<List<Board>>(articles, HttpStatus.OK);
 	}
 
-	@GetMapping("/write")
-	public String write() {
-		log.debug("board................... write 화면으로 이동");
-		return "board/InfoBoardWrite";
-	}
-
+//	@GetMapping("/write")
+//	public String write() {
+//		log.debug("board................... write 화면으로 이동");
+//		return "board/InfoBoardWrite";
+//	}
+	@ApiOperation(value = "게시판 글작성", notes = "새로운 게시글 정보를 입력한다. 그리고 DB입력 성공여부에 따라 'success' 또는 'fail' 문자열을 반환한다.", response = String.class)
 	@PostMapping("/write")
-	public String write(Board board, HttpSession session, RedirectAttributes redirectAttributes) throws Exception {
+	public ResponseEntity<String> writeArticle(Board board, HttpSession session, RedirectAttributes redirectAttributes) throws Exception {
 		User user = (User) session.getAttribute("userInfo");
 		board.setId(user.getId());
 		log.debug("Board............................write article:" + board);
+		
 		boardService.writeArticle(board);
-		redirectAttributes.addAttribute("pgno", "1");
-		redirectAttributes.addAttribute("key", "");
-		redirectAttributes.addAttribute("word", "");
-		return "redirect:/article/list";
+//		redirectAttributes.addAttribute("pgno", "1");
+//		redirectAttributes.addAttribute("key", "");
+//		redirectAttributes.addAttribute("word", "");
+		return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 	}
 
 	// protected void doGet(HttpServletRequest request, HttpServletResponse
--- src/main/java/com/ssafy/enjoytrip/controller/UserController.java
+++ src/main/java/com/ssafy/enjoytrip/controller/UserController.java
@@ -1,83 +1,138 @@
 package com.ssafy.enjoytrip.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.enjoytrip.model.dto.User;
 import com.ssafy.enjoytrip.model.service.UserService;
 
-@Controller
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
 @RequestMapping("/user")
+@Api("사용자 컨트롤러  API V1")
 public class UserController {
 
 	private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserService userService;
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
 
 	@ExceptionHandler(Exception.class)
 	public String handler(Exception ex, Model model) {
 		model.addAttribute("msg", ex.getMessage());
 		return "redirect:/index";
 	}
-
+	
+	@ApiOperation(value = "로그인", notes = "로그인 결과 메세지를 반환한다.")
 	@PostMapping("/login")
-	public String login(String id, String pw, HttpSession session) {
+	public ResponseEntity<Map<String, Object>> login(String id, String pw, HttpSession session) {
 		logger.debug("User............................login id:" + id);
 		logger.debug("User............................login pw:" + pw);
-
-		User user = userService.login(id, pw);
-		session.setAttribute("userInfo", user);
-		return "redirect:/index";
+		
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			User user = userService.login(id, pw);
+			if (user != null) {
+				resultMap.put("message", SUCCESS);
+				status = HttpStatus.ACCEPTED;
+			} else {
+				resultMap.put("message", FAIL);
+				status = HttpStatus.ACCEPTED;
+			}
+		} catch (Exception e) {
+			logger.error("로그인 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-
+	
+	@ApiOperation(value = "로그아웃", notes = "로그아웃 결과 메세지를 반환한다.")
 	@GetMapping("/logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
+	public ResponseEntity<Map<String, Object>> logout(HttpSession session) {
 		logger.info("User............................logout");
-		return "redirect:/index";
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			session.invalidate();
+			resultMap.put("message", SUCCESS);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			logger.error("로그아웃 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 	
-	@GetMapping("/mypage")
-	public String mypage(HttpSession session) {
-		logger.info("User............................mypage");
-		return "user/MyPage";
-	}
+//	@GetMapping("/mypage")
+//	public String mypage(HttpSession session) {
+//		logger.info("User............................mypage");
+//		return "user/MyPage";
+//	}
 	
-	@GetMapping("/signup")
-	public String signup(HttpSession session) {
-		logger.info("User............................signup");
-		return "user/SignUp";
-	}
+//	@GetMapping("/signup")
+//	public String signup(HttpSession session) {
+//		logger.info("User............................signup");
+//		return "user/SignUp";
+//	}
 	
+	@ApiOperation(value = "로그아웃", notes = "로그아웃 결과 메세지를 반환한다.")
 	@PostMapping("/regist")
-	public String regist(User regiUser, HttpSession session) {
+	public ResponseEntity<Map<String, Object>> regist(User regiUser, HttpSession session) {
 		logger.debug("User............................regist user:" + regiUser);
-
-		userService.regist(regiUser);
-		return "redirect:/index";
-	}
-
-	@GetMapping("/modifyForm")
-	public String modifyForm(HttpSession session) {
-		logger.info("User............................modifyForm으로 이동");
-		return "user/UserInfoModify";
+		
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+		try {
+			userService.regist(regiUser);
+			resultMap.put("message", SUCCESS);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			logger.error("로그인 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 	
+//	@GetMapping("/modifyForm")
+//	public String modifyForm(HttpSession session) {
+//		logger.info("User............................modifyForm으로 이동");
+//		return "user/UserInfoModify";
+//	}
+	
+	@ApiOperation(value = "유저 정보 수정", notes = "수정 결과 메세지를 반환한다.")
 	@PostMapping("/modify")
-	public String modify(User modiUser, HttpSession session) {
+	public ResponseEntity<Map<String, Object>> modify(User modiUser, HttpSession session) {
 		logger.info("User............................modify		 modiuser:" + modiUser);
 		User user = (User)session.getAttribute("userInfo");
 		logger.debug(user.toString());
+		
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
 		
 		String modiAtt = modiUser.getPass();
 		if(modiAtt != null && modiAtt != "")
@@ -98,16 +153,26 @@ public class UserController {
 		logger.info("User............................modify		 modiuser:" + user);
 		
 		userService.update(user);
-		return "user/MyPage";
+		resultMap.put("message", SUCCESS);
+		status = HttpStatus.ACCEPTED;
+		
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 	
+	@ApiOperation(value = "유저 정보 수정", notes = "수정 결과 메세지를 반환한다.")
 	@GetMapping("/deleteAccount")
-	public String deleteAccount(HttpSession session) {
+	public ResponseEntity<Map<String, Object>> deleteAccount(HttpSession session) {
 		User user = (User)session.getAttribute("userInfo");
 		logger.info("User............................delete" + user.getId());
+		
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
 
 		userService.remove(user.getId());
 		session.invalidate();
-		return "redirect:/index";
+		resultMap.put("message", SUCCESS);
+		status = HttpStatus.ACCEPTED;
+		
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
 }
